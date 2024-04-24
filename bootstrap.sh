635b6d42959230db750597f5d23b1427e52c7f25
Ghilman Aji <139083228+ghilmanaji@users.noreply.github.com>
2024-04-24 15:23:01 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 export LIBP2P_FORCE_PNET=1
 ipfs bootstrap rm --all
-ipfs bootstrap add /ip4/0.0.0.0/tcp/4001/ipfs/12D3KooWPhNbzobvBJJgPLnDqAvx5vgAPzaqGw9DZnMSE9cp3qez
+#dibawah ini adalah contoh untuk menambahkan alamat bootstrap peer ID
+# "ipfs bootstrap add /ip4/10.20.30.40/tcp/4001/ipfs/12D3KooWPhNbzobvBJJgPLnDqAvx5vgAPzaqGw9DZnMSE9cp3qez"
+# "ipfs bootstrap add /ip4/10.11.12.13/tcp/4001/ipfs/12D3KooWF38GXtmJ1phPXKjZ3je2shaSiQSW1FVvArnpWgnz8eCf"
+#masukan alamat bootstrap yang didapatkan dari file excel dibawah ini sesuai dengan contoh diatas
